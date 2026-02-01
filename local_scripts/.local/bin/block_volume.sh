334b6fbd339d4a6ebd8f068a5a794984dd828aa0
Alec Davis <unlikelytitan@gmail.com>
2026-02-01 09:57:15 -0700

    added checking for if the device is toggled or not

--- local_scripts/.local/bin/block_volume.sh
+++ local_scripts/.local/bin/block_volume.sh
@@ -7,18 +7,26 @@ esac
 vol=$(amixer sget Master | awk 'NR == 6 { print $5 }')
 vol=${vol:1:-2}
 
-if [ "$vol" -gt "99" ]; then
-	icon="🔊"
-elif [ "$vol" -gt "70" ]; then
-	icon="🔊 "
-elif [ "$vol" -gt "30" ]; then
-	icon="🔉 "
-elif [ "$vol" -gt "9" ]; then
-	icon="🔉 "
-elif [ "$vol" -gt "0" ]; then
-	icon="🔈  "
-else
+mute_state="$(amixer sget Master | awk 'NR == 6 { print $6 }')"
+mute_state=${mute_state:1:-1}
+
+if [ "$mute_state" == "on" ]; then
+    if [ "$vol" -gt "99" ]; then
+        icon="🔊"
+    elif [ "$vol" -gt "70" ]; then
+        icon="🔊 "
+    elif [ "$vol" -gt "30" ]; then
+        icon="🔉 "
+    elif [ "$vol" -gt "9" ]; then
+        icon="🔉 "
+    elif [ "$vol" -gt "0" ]; then
+        icon="🔈  "
+    else
         echo " 🔇  $vol%" && exit
+    fi
+elif [ "$mute_state" == "off" ]; then
+    echo " 🔇  $vol%" && exit
 fi
+
 
 echo "$icon$vol%"
