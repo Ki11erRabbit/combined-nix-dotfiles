a54508a8c757629ec295e0ffc903ce260df925d4
Alec Davis <unlikelytitan@gmail.com>
2026-01-31 17:54:22 -0700

    fixed desktop wallpaper script

--- scripts/desktop/wallpaper-wayland.sh
+++ scripts/desktop/wallpaper-wayland.sh
@@ -9,33 +9,33 @@ tallpapers_list="$HOME/tallpapers_list.txt"
 # $2 wallpaper list
 # $3 wallpaper dir
 function select_wallpaper () {
-    if [[ $(stat --printf="%s" "$2") == "0" ]]; then
-        find "$3" -name "*.png" -or -name "*.jpeg" -or -name "*.jpg" -or -name "*.webm" -or -name "*.mp4" | shuf > "$2"
-        file=$(cat $2 | head -n 1)
-    else 
-        file=$(cat $2 | head -n 1)
+    if [[ ! -s "$2" ]]; then
+        find "$3" -name "*.png" -o -name "*.jpeg" -o -name "*.jpg" -o -name "*.webm" -o -name "*.mp4" | shuf > "$2"
     fi
-    exec 19<> "$2"
-    tail -n +2 "$2" >&19 
-    exec 19>&-
-    echo "$1: $file" >> $HOME/wallpaper.log
+    
+    file=$(head -n 1 "$2")
+    
+    # Use a temp file to avoid corruption
+    tail -n +2 "$2" > "$2.tmp"
+    mv "$2.tmp" "$2"
+    
+    echo "$1: $file" >> "$HOME/wallpaper.log"
     echo "$file"
-
 }
 
-file1=$(select_wallpaper "wide1" $widepapers_list $widepapers)
-file2=$(select_wallpaper "wide2" $widepapers_list $widepapers)
-file3=$(select_wallpaper "tall" $tallpapers_list $tallpapers)
+file1=$(select_wallpaper "wide1" "$widepapers_list" "$widepapers")
+file2=$(select_wallpaper "wide2" "$widepapers_list" "$widepapers")
+file3=$(select_wallpaper "tall" "$tallpapers_list" "$tallpapers")
 
 # $1 display
 # $2 file
 function apply_wallpaper () {
     case $2 in
         *.png|*.jpeg|*.jpg)
-            swaybg -o $1 -i "$2" -m fill  &
+            swaybg -o "$1" -i "$2" -m fill &
             ;;
         *.webm|*.mp4)
-            mpvpaper -o "no-audio loop" $1 "$2" &
+            mpvpaper -o "no-audio loop" "$1" "$2" &
             ;;
     esac
 }
@@ -43,7 +43,7 @@ function apply_wallpaper () {
 pkill swaybg
 pkill mpvpaper
 pkill .mpvpaper-wrapp
+
 apply_wallpaper "DP-1" "$file1"
 apply_wallpaper "DP-2" "$file2"
 apply_wallpaper "HDMI-A-1" "$file3"
-
