acbb7883396b39c61ac08e3ca0e4975effe7a111
Alec Davis <unlikelytitan@gmail.com>
2026-04-13 21:14:38 -0600

    added t480 keyboard setup

--- hosts/common.nix
+++ hosts/common.nix
@@ -309,5 +309,38 @@ in {
     };
 
     system.stateVersion = "25.11"; # Did you read the comment?
+
+    
+    services.udev.extraRules = ''
+        # for kmonad to work
+        KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+    '';
+    security.sudo = {
+        enable = true;
+        extraRules = [{
+            commands = [{
+                command = "${pkgs.haskellPackages.kmonad}/bin/kmonad";
+                options = [ "NOPASSWD" ];
+                }
+                {
+                command = "/run/current-system/sw/bin/kmonad";
+                options = [ "NOPASSWD" ];
+                }
+                {
+                command = "${pkgs.procps}/bin/pkill kmonad";
+                options = [ "NOPASSWD" ];
+                }
+                {
+                command = "/run/current-system/sw/bin/pkill /run/current-system/sw/bin/kmonad";
+                options = [ "NOPASSWD" ];
+                }
+                {
+                command = "${pkgs.procps}/bin/pkill ${pkgs.haskellPackages.kmonad}/bin/kmonad";
+                options = [ "NOPASSWD" ];
+                }
+                ];
+            groups = [ "wheel" ];
+        }];
+    };
 }
 
--- scripts/think-nix-t480/setup-keyboard.sh
+++ scripts/think-nix-t480/setup-keyboard.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+
+sudo kmonad /home/ki11errabbit/.config/kmonad/t480.kbd
